eba83f46e1821969595bd3778228b958efad7e9f
George Hilios <ghilios@gmail.com>
2024-05-04 23:55:36 -0400

    Ensure correct uneven hyperbola fitting method used when displaying previous AF runs

--- Joko.NINA.Plugins/Joko.NINA.Plugins.HocusFocus/Properties/AssemblyInfo.cs
+++ Joko.NINA.Plugins/Joko.NINA.Plugins.HocusFocus/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.0.0.15")]
-[assembly: AssemblyFileVersion("3.0.0.15")]
+[assembly: AssemblyVersion("3.0.0.16")]
+[assembly: AssemblyFileVersion("3.0.0.16")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Hocus Focus")]
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.3001")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.9001")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
